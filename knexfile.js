b7a82048d2a8385d303c13a283ac740ddecbaa03
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-06-02 04:30:44 -0500

    See if Prod was missing

--- knexfile.js
+++ knexfile.js
@@ -18,8 +18,24 @@ module.exports = {
     seeds: {
       directory: './data/seeds'
     }
-  }
-}
+  },
+
+
+   production: {
+   client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+    database: 'tasks',
+    user: 'postgres',
+      password: 'Scorpio1978',
+  },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: { directory: './data/seeds' },
+  },
+};
+
 
 
 
