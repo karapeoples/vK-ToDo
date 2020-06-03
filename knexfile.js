457eb8a09cd74274c4d8672bf976ead6c8f60647
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-06-03 01:54:14 -0500

    Oopsie Typo

--- knexfile.js
+++ knexfile.js
@@ -15,8 +15,6 @@ module.exports = {
 	},
 
 	// update the following configuration to use PostgreSQL
-	production: {
-		client: 'pg',
 		production: {
 			client: 'pg',
 			connection: process.env.DATABASE_URL,
@@ -28,11 +26,6 @@ module.exports = {
 			},
 		},
 	}
-}
-
-
-// postgres://axpibcvvmtqmxy:79bac4b3f1ae2ff559a7781665bfbd8b21f7e4a3fb00f83df1e10644d366ed07@ec2-34-232-147-86.compute-1.amazonaws.com:5432/dbnslsb72uusfm
-  
 
 
 
