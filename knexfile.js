3ed77cfae156786fa77f0a79f407c914ffe88207
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-06-03 01:39:10 -0500

    Trying to Deploy the Backend

--- knexfile.js
+++ knexfile.js
@@ -1,22 +1,38 @@
 // Update with your config settings.
-
-require('dotenv').config()
-
-// DATABASE_URL env should follow this format:
-// postgres://user_name:password@ipaddress:port/table
-// Example: postgres://jimmy:password@localhost:5432/pg_database
-
 module.exports = {
 	development: {
-		client: 'pg',
-		// 🔻 Points to our local Postgresql database
-		connection: process.env.DATABASE_URL,
+		client: 'sqlite3',
+		connection: {
+			filename: './data/errands.db3',
+		},
+		useNullAsDefault: true,
+		migrations: {
+			directory: './data/migrations',
+		},
+		seeds: {
+			directory: './data/seeds',
+		},
 	},
+
+	// update the following configuration to use PostgreSQL
+	production: {
+		client: 'pg',
+		production: {
+			client: 'pg',
+			connection: process.env.DATABASE_URL,
+			migrations: {
+				directory: './data/migrations',
+			},
+			seeds: {
+				directory: './data/seeds',
+			},
+		},
+	}
 }
 
 
-
-
-
+// postgres://axpibcvvmtqmxy:79bac4b3f1ae2ff559a7781665bfbd8b21f7e4a3fb00f83df1e10644d366ed07@ec2-34-232-147-86.compute-1.amazonaws.com:5432/dbnslsb72uusfm
   
+
+
 
